2edb5145e68474732f50545045bc3361d0271918
Alex <act65@Alexanders-iMac.local>
2016-05-19 01:37:04 +1200

    thinking about math operations

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # FAT
-Functional Arrays and Tensors. Fatrix - a functional matrix.
-An extension of numpy libraries with matrices to allow a functional type of matrix algebra. 
-Can be used to build computational graphs ..
-Also, an excuse for me to learn about functional programming and some higher order math.
+
+Functional Arrays and Tensors. 
+Fatrix: a matrix of functions.
+Fensor: a tensor of functions.
+
+Goal: To design a framework for thinking about networks/graphs as structured arrays.
+Why: To have a formal toolset for understanding structure in networks. To help answer the question of how structure and function are related.
+What: An extension of numpy libraries (ndarrays) to allow a functional type of linear algebra on matricies/tensors. 
 
     Traditional dot
     a, b  .  e,  f   =  a.e + b.g, a.f + b.h
@@ -11,12 +15,17 @@ Also, an excuse for me to learn about functional programming and some higher ord
     a, b  .  e,  f   =  a(e) + b(g), a(f) + b(h)
     c, d     g,  h   =  c(e) + d(g), c(f) + d(h)
     
-The motivation being that when you look at the fatrixes of common neural network structures, like CNNs, RNNs, ResNets, … etc you start to see some interesting patterns in the structure of the fatrices. See [this](https://github.com/act65/FAT/blob/master/Modular%20nets.ipynb) for more details and motivation.
+Potential applications
+* Engineering: Can be used to easily build computational graphs (e.g. in tensorflow).
+* Science: The structure of common neural network, like CNNs, RNNs, ResNets, show some interesting patterns when represented with FAT. See [this](https://github.com/act65/FAT/blob/master/Modular%20nets.ipynb) for some examples.
+* Math: ?? how does this contribute to math literature? See [this]() for a formal definition of the langauge.
 
 
 # To-do
 ### Need
 * Decide on and define algebra on fatrixes
+    * Composition
+    * ??
 * Generalise to tensors
 * Formulate the problem it is solving, make sure this is well motivated
 * Make working examples for well known/interesting architectures
--- Simple2D.py
+++ Simple2D.py
@@ -1,6 +1,6 @@
 import numpy as np
 import types
-class Fatrix():
+class Fatrix(object):
     """ 
     Using the matrix relations, make a computational graph.
     Use n-d matrix multiplication on (func , var) to make the graph.
@@ -10,29 +10,23 @@ class Fatrix():
     #all funcs need a _call method.
     #and recieve only one input argument.
     """
-    def __init__(self,shape = None,functions = None):
-
-        self.func = functions
+    def __init__(self,shape = None):
         self.shape = shape
-        
+        #generate a matrix of shape and
+        #fill it with zeros/identity functions.
         self.func = np.empty(shape,types.FunctionType)
-        
-        self.make()
-        
-    def make(self):
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
                 if self.func[i,j] == 0 or self.func[i,j] == None:
                     self.func[i,j] = self.o
         
-    def o(self,x):#a function for null cases. this doesnt seem efficient?
+    def o(self,x):
+        #called o because it helps with the printing. should fix
+        #a function for zero/null/identity cases. this doesnt seem efficient?
         return 0
         
     def __call__(self,x):
         return Fdot(self.func,x) #need to generalise to tensors.
-    
-    def __add__(self,x): #hmmm
-        return x
     
     def __setitem__(self,i,x):
         self.func[i] = x
@@ -52,6 +46,27 @@ class Fatrix():
                 string += self.func[i,j].__name__ + ' '
             string += '\n'
         return string
+    
+    #The math functions 
+    #https://docs.python.org/3/library/operator.html
+    #What is the point of using numpy as
+    #I have to redefine these anyway...
+    def __add__(self,x): #hmmm
+        return x
+    def __sub__(self,x):
+        pass
+    def __pos__(self,x):
+        pass
+    def __neg__(self,x):
+        pass
+    def __mul__(self,x):
+        pass
+    def __matmul__(self,x):
+        pass
+    def __div__(self,x):
+        pass
+    def __pow__(self,x):
+        pass
 
 def Fdot(x,y):
     """
@@ -66,7 +81,10 @@ def Fdot(x,y):
     c, d     g,  h   =  c(e) + d(g), c(f) + d(h)
 
     Inputs:
-    x - A n by m 2d matrix of functions
+    x - A n by m 2d matrix of functions. 
+    Each function must have a call method and only expect
+    one input variable.
+    
     y - A m by p 2d matrix of variables
 
     Returns:
